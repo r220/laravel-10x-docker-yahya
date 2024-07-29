f45c9f5b06757af65671e3e6acddf0014a49c71b
reem <upontime000@gmail.com>
2024-07-29 03:52:55 +0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
-  <link rel="stylesheet" href="yahya.css" />
-  <script src="yahya.js"></script>
+  <link rel="stylesheet" href="{{ HTML::url('yahya.css') }}" />
+  <script src="{{ HTML::url('yahya.js') }}"></script>
 </head>
 
 <body>
