012985d9a1b0bc3cc0477093d2c1742d74a6a70b
reem <upontime000@gmail.com>
2024-08-25 15:18:51 +0300

    update db

--- public/connection2.php
+++ public/connection2.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = pg_connect("host=dpg-cqfs4j9u0jms7387eiu0-a.oregon-postgres.render.com port=5432 dbname=yahyadb user=root password=6RHocZ4oxHzeJBPLQoCd5u5G0IwZRjM2");
+$conn = pg_connect("host=dpg-cr5c2fij1k6c7394csug-a.oregon-postgres.render.com port=5432 dbname=yahyadb_ta8l user=root password=tDzW5XB3ck97zpkDkFRlKmp8vEDCPX7A");
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
